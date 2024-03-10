72e0aa9e71c17944d2740d79da16273a0467b95d
zoe <593690155@qq.com>
2024-03-10 20:41:47 +0800

    fix: 启用gzip

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { isProduction } from './config';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   if (!isProduction) {
     app.enableCors();
   }
+  app.use(compression());
   app.setGlobalPrefix('/v1/');
   await app.listen(8080);
 }
