17dfcfdd751466c11e831c98458d940b5345afac
zoe <593690155@qq.com>
2024-03-10 11:37:15 +0800

    fix: 修改path

--- pm2.config.js
+++ pm2.config.js
@@ -12,7 +12,7 @@ module.exports = {
       host: '43.153.50.117',
       ref: 'origin/feature',
       repo: 'git@github.com:ava-grace-zoe/zoe.git',
-      path: '/home/ubuntu/project/zoe',
+      path: '/home/ubuntu/project/pm2',
       'post-deploy': 'yarn && echo successfully!',
     },
   },
