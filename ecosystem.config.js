27813012319cdfb27fcf769883e2d65508ad63d8
zoe <593690155@qq.com>
2024-03-18 22:51:20 +0800

    build: 修改部署方式

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,8 @@ module.exports = {
       ref: 'origin/feature',
       repo: 'git@github.com:ava-grace-zoe/zoe.git',
       path: '/home/ubuntu/project/zoe',
-      'post-deploy': 'yarn && yarn build  && yarn inject-path && yarn pm2start',
+      'post-deploy':
+        'export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.11.1/bin &&  yarn && yarn build  && yarn inject-path && yarn pm2start',
     },
   },
 };
