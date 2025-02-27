7f33b8de2d171a75c3f0ab74ca090108e9786a10
chenglanbin <chenglanbin@sparkedu.com>
2025-02-27 17:33:11 +0800

    feat: add

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,29 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 
 import { TOKEN } from './config';
-
+const lastMessage: unknown[] = [];
 @Controller()
 export class AppController {
   @Post('login')
   login(@Body() { password }: { password: string }) {
     return password === TOKEN;
+  }
+
+  @Post('llm')
+  llm(@Req() req: Request, @Body() body: { body: string }) {
+    lastMessage.push({
+      body,
+      headers: req.headers,
+    });
+    console.log(req.headers, body);
+    return {
+      body,
+      headers: req.headers,
+    };
+  }
+
+  @Get('lastMessage')
+  getLastMessage() {
+    return lastMessage;
   }
 }
